69c45ae6b4401b915684a0c56a528d82bf933ffb
Naveen Mathew <naveenmathew1585@gmail.com>
2024-10-09 20:23:59 +0530

    ✨ add dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,48 @@
-<script>
+<script lang="ts">
 	import '@fontsource-variable/rethink-sans';
 	import '../app.css';
+
+	import { ModeWatcher } from 'mode-watcher';
+	import { resetMode, setMode } from 'mode-watcher';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
 </script>
 
 <nav
-	class="fixed z-40 flex h-14 w-full items-center border-b-2 border-opacity-85 bg-background bg-opacity-75 px-10 backdrop-blur-md"
+	class="fixed z-40 flex h-14 w-full items-center justify-between border-b-2 border-opacity-85 bg-background bg-opacity-75 px-10 backdrop-blur-md"
 >
-	<div class="flex items-center gap-3">
-		<img class="h-8" src="/favicon.svg" alt="scammer list icon" />
+	<a href="/" class="flex items-center gap-3">
+		<img
+			class="h-8 rounded-full border-2 border-primary"
+			src="/favicon.svg"
+			alt="scammer list icon"
+		/>
 		<div class="text-2xl font-semibold tracking-tight">Scammer Patrol</div>
-	</div>
+	</a>
+	<DropdownMenu.Root>
+		<DropdownMenu.Trigger asChild let:builder>
+			<Button builders={[builder]} variant="secondary" size="icon">
+				<Sun
+					class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+				/>
+				<Moon
+					class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+				/>
+				<span class="sr-only">Toggle theme</span>
+			</Button>
+		</DropdownMenu.Trigger>
+		<DropdownMenu.Content align="end">
+			<DropdownMenu.Item on:click={() => setMode('light')}>Light</DropdownMenu.Item>
+			<DropdownMenu.Item on:click={() => setMode('dark')}>Dark</DropdownMenu.Item>
+			<DropdownMenu.Item on:click={() => resetMode()}>System</DropdownMenu.Item>
+		</DropdownMenu.Content>
+	</DropdownMenu.Root>
 </nav>
 <div class="min-h-dvh px-10 pt-20">
+	<ModeWatcher />
 	<slot></slot>
 </div>
 
