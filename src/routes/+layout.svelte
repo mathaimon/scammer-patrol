2fdbf238e04e9d70c80a0714d5141195862417c4
Naveen Mathew <naveenmathew1585@gmail.com>
2024-10-09 16:09:31 +0530

    🎉 started scammer list

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '@fontsource-variable/rethink-sans';
+	import '../app.css';
+</script>
+
+<slot></slot>
+
+<style>
+	:global(body) {
+		font-family: 'Rethink Sans Variable', sans-serif;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<div class="flex h-dvh w-full flex-col items-center justify-center">
+	<img class="mb-4 h-32 rounded-full border-4 border-primary" src="/favicon.svg" alt="icon" />
+	<h1 class="text-4xl font-bold underline decoration-primary">Scammer List</h1>
+	<p class="text-xl">Name and shame the scammers</p>
+</div>
